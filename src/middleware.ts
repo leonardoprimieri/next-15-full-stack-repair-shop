97a4d1d04033aaf632737fa0d401e790bd2635fe
Leonardo Merlo Primieri <leonardo.mprimieri@gmail.com>
2026-01-04 14:12:38 -0300

    Add database integration with Neon and Drizzle ORM, create schema for customers and tickets, and update middleware for Kinde authentication.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,8 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
-import { NextRequest } from "next/server";
 
-export default withAuth(
-  async function middleware(request: NextRequest) {
-    //console.log(request)
-  },
-  {
-    isReturnToCurrentPage: true,
-  }
-);
+export default withAuth(async function middleware() {}, {
+  isReturnToCurrentPage: true,
+});
 
 export const config = {
   matcher: [
